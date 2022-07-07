57bdad542a38fb186307cbac2bfa3819a13cfdfb
Alejandro Gaytan <alejandro.gaytan5785@alumnos.udg.mx>
2022-07-07 18:23:41 -0500

    Creación del servidor con express

---
+++ Index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+/**
+ * * Routing
+ */
+app.get('/', (req, res) => {
+    res.json({msg:'Prueba de routing'});
+});
+
+/**
+ * * Definición del puerto del proyecto
+ */
+app.listen(3000, () => {
+    console.log(`El servidor esta funcionando correctamente`)
+}) 
