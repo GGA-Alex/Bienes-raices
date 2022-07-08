ccee065a7d86de8fa0bd99bb2784096f74ea3268
Alejandro Gaytan <alejandro.gaytan5785@alumnos.udg.mx>
2022-07-08 10:50:33 -0500

    Creación de validaciones ene l formulario de registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,6 @@
+import { check, validationResult } from 'express-validator';
+import Usuario from '../models/Usuario.js';
+
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
         pagina: 'Iniciar sesión'
@@ -10,8 +13,27 @@ const formularioRegistro = (req, res) => {
     });
 }
 
-const registrar = (req, res) => {
-    console.log(req.body);
+const registrar = async (req, res) => {
+
+    await check('nombre').notEmpty().withMessage('El Nombre no puede ir vacio').run(req);
+    await check('email').isEmail().withMessage('Forma de email incorrecta').run(req);
+    await check('password').isLength({min:6}).withMessage('El password debe de tener al menos 6 caracteres').run(req);
+    
+    let resultado = validationResult(req);
+
+    if(!resultado.isEmpty()) {
+        return res.render('auth/registro', {
+            pagina: 'Crear cuenta',
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
+        });
+    }
+    
+    const usuario = await Usuario.create(req.body);
+    res.json(usuario);
 }
 
 const formularioOlvidePassword = (req, res) => {
